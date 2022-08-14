fcccc7a2eac1a9e756255eb73631517d1dc83170
David Hockley <david.hockley@gmail.com>
2022-08-14 08:54:53 +0200

    first update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,24 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import astroI18next from "astro-i18next";
+import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
   site: "https://tailwind-astro.vercel.app",
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap({}),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), sitemap({}), astroI18next({
+    baseLanguage: "en",
+    i18next: {
+      debug: false,
+      // convenient during development to check for missing keys
+      supportedLngs: ["en", "fr"]
+    }
+  }), mdx(), image()]
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+    /*colors: {
+      ...colors,
+      black: colors.black,
+      white: colors.white,
+      gray: {...colors.gray, '100':"#FAF9F8", '200':"#ededed" }, //"#e2e2e2
+      red:           '#ff5d55',
+      orange:        '#FFAC58',
+      yellow:        '#ffa834',
+      darkgreen:     '#469936',
+      green:         '#8FC01F',
+      lightgreen:    '#BBD877',
+      logogray:      '#575756',
+      hmred:         '#d6001c',
+      beige:         '#EEEAE5',
+      darkgray:      '#434343',
+      dark:          '#3D3D3D',
+    },*/
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/typography')],
 	darkMode: 'class',
 }
